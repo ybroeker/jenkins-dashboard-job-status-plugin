a670430afe5a27403d1bb3b7db05870467641aac
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2019-11-14 12:50:10 +0100

    Fix NPE, if a job is unbuilt

--- src/main/java/de/ybroeker/jenkins/dashboard/jobstatus/JobStatusPortlet.java
+++ src/main/java/de/ybroeker/jenkins/dashboard/jobstatus/JobStatusPortlet.java
@@ -13,14 +13,10 @@ import org.kohsuke.stapler.DataBoundConstructor;
 
 public class JobStatusPortlet extends DashboardPortlet {
 
-    private final NumberFormat percentageFormat;
 
     @DataBoundConstructor
     public JobStatusPortlet(String name) {
         super(name);
-        percentageFormat = NumberFormat.getPercentInstance(Locale.ENGLISH);
-        percentageFormat.setMinimumFractionDigits(0);
-        percentageFormat.setMaximumFractionDigits(2);
     }
 
     public Map<BallColor, Integer> getBuildStat(List<TopLevelItem> jobs) {
@@ -29,8 +25,12 @@ public class JobStatusPortlet extends DashboardPortlet {
         for (TopLevelItem job : jobs) {
             if (job instanceof Job) {
                 final Run<?, ?> lastBuild = ((Job<?, ?>) job).getLastBuild();
+                if (lastBuild == null) {
+                    colStatBuilds.merge(BallColor.NOTBUILT, 1, Integer::sum);
+                    continue;
+                }
                 BallColor bColor = lastBuild.getIconColor();
-                if (bColor != null && bColor.noAnime() != null && colStatBuilds.get(bColor) != null) {
+                if (bColor != null && bColor.noAnime() != null) {
                     colStatBuilds.merge(bColor.noAnime(), 1, Integer::sum);
                 }
             }
@@ -40,7 +40,14 @@ public class JobStatusPortlet extends DashboardPortlet {
 
     public String getPercentage(int value, int nBuilds) {
         float fraction = nBuilds != 0 ? (((float) value) / nBuilds) : 0;
-        return percentageFormat.format(fraction);
+        return getPercentageFormat().format(fraction);
+    }
+
+    private NumberFormat getPercentageFormat() {
+        NumberFormat percentageFormat= NumberFormat.getPercentInstance(Locale.ENGLISH);
+        percentageFormat.setMinimumFractionDigits(0);
+        percentageFormat.setMaximumFractionDigits(2);
+        return percentageFormat;
     }
 
     @Extension
--- src/test/java/de/ybroeker/jenkins/dashboard/jobstatus/JobStatusPortletTest.java
+++ src/test/java/de/ybroeker/jenkins/dashboard/jobstatus/JobStatusPortletTest.java
@@ -1,11 +1,20 @@
 package de.ybroeker.jenkins.dashboard.jobstatus;
 
+import java.util.*;
+
+import hudson.model.BallColor;
+import hudson.model.FreeStyleProject;
+import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class JobStatusPortletTest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
 
     JobStatusPortlet jobStatusPortlet = new JobStatusPortlet("-");
 
@@ -31,6 +40,39 @@ public class JobStatusPortletTest {
     public void shouldNotFailOnZeroBuilds() {
         final String percentage = jobStatusPortlet.getPercentage(0, 0);
         assertThat(percentage).isEqualTo("0%");
+    }
+
+    @Test
+    public void shouldNotFailOnUnbuiltJobs() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        final Map<BallColor, Integer> buildStat = jobStatusPortlet.getBuildStat(Collections.singletonList(project));
+
+        assertThat(buildStat)
+                .hasSize(1)
+                .containsEntry(BallColor.NOTBUILT, 1)
+        ;
+    }
+
+    @Test
+    public void shouldCalculateCorrectStatsForSIngleBlueRun() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        j.assertBuildStatusSuccess(project.scheduleBuild2(0));
+        assertThat(project.getLastBuild()).isNotNull();
+        final Map<BallColor, Integer> buildStat = jobStatusPortlet.getBuildStat(Collections.singletonList(project));
+
+        assertThat(buildStat)
+                .hasSize(1)
+                .containsEntry(BallColor.BLUE, 1)
+        ;
+    }
+
+    @Test
+    public void shouldNotFailOnNullJob() throws Exception {
+        final Map<BallColor, Integer> buildStat = jobStatusPortlet.getBuildStat(Collections.singletonList(null));
+
+        assertThat(buildStat)
+                .isEmpty()
+        ;
     }
 
 }
